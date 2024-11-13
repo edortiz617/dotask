f55a96cd47e3e00398df73b940d9f3512b6e513d
Edwin Ortiz <e.ortiz617@yahoo.com>
2024-11-12 19:49:21 -0500

    Do task web services

---
+++ index.js
@@ -0,0 +1,31 @@
+const express = require('express');
+//Setup express app
+const app = express();
+
+const PORT = process.env.PORT || 3000;
+
+function doWork (task) {
+    return `Task ${task} is done!`
+}
+
+//Config your routes here
+app.get('/doWork', (req, res) => {
+    //Input
+    const task = req.query.task;
+    //doWork
+    const result = doWork(task);
+    //Output
+    //Server Output
+    console.log(result);
+
+    //Client Output
+    // res.send(result)
+    response.json({
+        message: result
+    })
+})
+
+app.get('/' , (req, res) => res.send('This is the default route.. Happy face'))
+
+
+app.listen(PORT, () => console.log `Server is running on port ${PORT}`);
